fe6a606eab1e64b5144842d0ca4668339986c61b
mattfwilson <mattfwilson@gmail.com>
2022-06-27 19:52:43 -0400

    Fixed the super ids and adding in list for card types

--- slay.py
+++ slay.py
@@ -1,28 +1,40 @@
-import random
 import itertools
+import time
+import operator
 
 hand = []
+card_types = ['Attack', 'Defend', 'Skill', 'Power', 'Curse']
 
 class Card:
+
     id = itertools.count(1).__next__
 
     def __init__(self):
         self.id = Card.id()
 
     def __repr__(self):
-        return f'Card: {self.id}'
+        return f'Card {self.id}'
 
 class Attack(Card):
-    def __init__(self, id):
+    def __init__(self):
+        super().__init__()
+        self. type = card_types[0]
         self.damage = 6
+    
+    def __repr__(self):
+        return f'{self.type} Card {self.id}'
 
 class Defend(Card):
-    def __init__(self, id):
+    def __init__(self):
+        super().__init__()
+        self.type = card_types[1]
         self.block = 6
+    
+    def __repr__(self):
+        return f'{self.type} Card {self.id}'
 
 hand.append(Card())
-hand.append(Card())
-hand.append(Card())
+hand.append(Attack())
+hand.append(Defend())
 
 print(hand)
-
