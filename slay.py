9e3a153ce69054cd4b6e746760d12c869d91ff6a
mattfwilson <mattfwilson@gmail.com>
2022-06-27 02:19:18 -0400

    Appending new cards to hand list

--- slay.py
+++ slay.py
@@ -1,13 +1,28 @@
+import random
+import itertools
+
+hand = []
+
 class Card:
-    def __init__(self, id):
-        self.id = id
+    id = itertools.count(1).__next__
+
+    def __init__(self):
+        self.id = Card.id()
+
+    def __repr__(self):
+        return f'Card: {self.id}'
 
 class Attack(Card):
     def __init__(self, id):
-        super().init(id)
         self.damage = 6
 
 class Defend(Card):
     def __init__(self, id):
-        super().init(id)
         self.block = 6
+
+hand.append(Card())
+hand.append(Card())
+hand.append(Card())
+
+print(hand)
+
