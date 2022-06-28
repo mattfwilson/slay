04be0358d441280d726362fa78c9443d3bab2dba
mattfwilson <mattfwilson@gmail.com>
2022-06-28 02:33:11 -0400

    Added attack and block attributes to respective cards. Added global vars for USER_LVL and SPIRE_LVL

--- slay.py
+++ slay.py
@@ -1,6 +1,8 @@
 import itertools
 import random
 
+USER_LVL = 1
+SPIRE_LVL = 1
 DECK_START = 8
 CURRENT_HAND = []
 DRAW_PILE = []
@@ -8,30 +10,35 @@ DISCARD_PILE = []
 CARD_TYPES = ['Base', 'Attack', 'Defend', 'Skill']
 
 class Card:
-
     id = itertools.count(0).__next__
 
     def __init__(self):
         self.id = Card.id()
         self.type = CARD_TYPES[0]
 
-    def __repr__(self):
-        return f'({self.id}) {self.type} Card'
-
 class Attack(Card):
     def __init__(self):
         super().__init__()
         self.type = CARD_TYPES[1]
+        self.attack = 6
+
+    def __repr__(self):
+        return f'({self.id}) {self.type} {self.attack}'
 
 class Defend(Card):
     def __init__(self):
         super().__init__()
         self.type = CARD_TYPES[2]
+        self.block = 6
+
+    def __repr__(self):
+        return f'({self.id}) {self.type} {self.block}'
 
 def createDeck():
     for i in range(4):
         CURRENT_HAND.append(Attack())
         CURRENT_HAND.append(Defend())
+    random.shuffle(CURRENT_HAND)
     print(CURRENT_HAND)
 
 createDeck()
