16c41e9644b628a1fff4aac4a2e75b73618491b6
mattfwilson <mattfwilson@gmail.com>
2022-06-29 00:30:51 -0400

    Made CURRENT_ENERGY, MAX_ENERGY, and TURN_COUNT vars global inside of startTurn function. Moved TURN_COUNT increment to endTurn function.

--- slay.py
+++ slay.py
@@ -4,12 +4,12 @@ import random
 USER_LVL = 1
 CURRENT_HP = 70
 MAX_HP = 70
-CURRENT_ENERGY = 3
+CURRENT_ENERGY = 1
 MAX_ENERGY = 3
 USER_GOLD = 0
 COMBAT_COUNT = 1
 TURN_COUNT = 1
-HAND_START = 5
+DRAW_COUNT = 5
 DRAW_PILE = []
 CURRENT_HAND = []
 DISCARD_PILE = []
@@ -41,27 +41,40 @@ class Defend(Card):
         return f'({self.id}) {self.type} {self.block}'
 
 def createDeck():
-    for i in range(5):
+    for i in range(DRAW_COUNT):
         DRAW_PILE.append(Attack())
         DRAW_PILE.append(Defend())
     random.shuffle(DRAW_PILE)
 
 def showSummary():
+    print(f'Turn: {TURN_COUNT}')
     print(f'HP: {CURRENT_HP}/{MAX_HP}')
     print(f'Energy: {CURRENT_ENERGY}/{MAX_ENERGY}')
     print(f'Cards: {CURRENT_HAND}')
+
+def startCombat():
+    pass
 
 def drawCards():
     for i in range(5):
         card = DRAW_PILE.pop(-1)
         CURRENT_HAND.append(card)
 
-def turnStart(energy, turn):
-    energy = MAX_ENERGY
-    turn += 1
+def startTurn():
+    global CURRENT_ENERGY
+    global TURN_COUNT
+    CURRENT_ENERGY = MAX_ENERGY
     drawCards()
     showSummary()
 
+def enemyTurn():
+    pass
+
+def endTurn():
+    CURRENT_HAND.pop(len(range(CURRENT_HAND)))
+    TURN_COUNT += 1
+    enemyTurn()
+    
 createDeck()
-turnStart(CURRENT_ENERGY, TURN_COUNT)
+startTurn()
 
