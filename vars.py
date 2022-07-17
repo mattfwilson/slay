039e734e7aa65adfbed1f319106957b6316e7f0d
mattfwilson <mattfwilson@gmail.com>
2022-07-17 16:02:01 -0400

    Reverted from list() to [] to empty lists for speed.

--- vars.py
+++ vars.py
@@ -5,12 +5,12 @@ MAX_HP = 50
 ENERGY = 5
 MAX_ENERGY = 5
 DRAW_COUNT = 5
-HAND = list()
-DRAW_PILE = list()
-DISCARD_PILE = list()
+HAND = []
+DRAW_PILE = []
+DISCARD_PILE = []
 
 # combat
-ENEMY = list()
+ENEMY = []
 COMBAT_COUNT = 1
 TURN_COUNT = 0
 ACTIONS = ['Draw', 'Attack', 'Block', 'Draw Pile', 'Discard Pile']
