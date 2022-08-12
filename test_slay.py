4e2d16ed70edd3f3a60bb1eabbbff7c4a0d5c0b1
mattfwilson <mattfwilson@gmail.com>
2022-08-12 03:00:24 -0400

    More attempts on figuring out unit tests.

---
+++ test_slay.py
@@ -0,0 +1,18 @@
+import unittest
+from vars import *
+
+class Test_Deck(unittest.TestCase):
+    
+    def test_HP(self):
+        self.assertGreater(state.HP, 35)
+
+    def test_energy(self):
+        self.assertEqual(state.ENERGY, 3)
+
+    def test_name(self):
+        if state.NAME is str():
+            res = True
+            self.assertTrue(res)
+
+if __name__ == '__main__':
+    unittest.main()
