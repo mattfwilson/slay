784417738cabc31f1b476716cc7a59147e3ce5ce
mattfwilson <mattfwilson@gmail.com>
2023-02-19 07:53:22 -1000

    Check for if random percentage matches dict values

--- event_chance.py
+++ event_chance.py
@@ -4,3 +4,10 @@ events = {'Event': .6375, 'Shrine': .2125, 'Fight': .10, 'Shop': .03, 'Treasure'
 
 for key, value in events:
     print(value)
+
+res = random.randint(.01, 1)
+print(res)
+
+for value in events:
+    if res == events.values():
+        print(events.key)
