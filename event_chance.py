511eefbd69f72510a7e877ec558f806e6b26e9bb
mattfwilson <mattfwilson@gmail.com>
2023-02-19 09:45:12 -1000

    Changed randint from decimal to integer

--- event_chance.py
+++ event_chance.py
@@ -2,10 +2,7 @@ import random
 
 event_chance = {'Event': .6375, 'Shrine': .2125, 'Fight': .10, 'Shop': .03, 'Treasure': .02}
 
-# for key, value in event_chance:
-#     print(value)
-
-res = random.randint(.01, 1)
+res = random.randint(1, 100)
 print(res)
 
 for value in event_chance:
