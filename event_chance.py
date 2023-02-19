0441dc3ea7ea512aae4be7cd009171ae6c276a85
mattfwilson <mattfwilson@gmail.com>
2023-02-19 07:59:23 -1000

    Commented out main loop

--- event_chance.py
+++ event_chance.py
@@ -1,13 +1,13 @@
 import random
 
-events = {'Event': .6375, 'Shrine': .2125, 'Fight': .10, 'Shop': .03, 'Treasure': .02}
+event_chance = {'Event': .6375, 'Shrine': .2125, 'Fight': .10, 'Shop': .03, 'Treasure': .02}
 
-for key, value in events:
-    print(value)
+# for key, value in event_chance:
+#     print(value)
 
 res = random.randint(.01, 1)
 print(res)
 
-for value in events:
-    if res == events.values():
-        print(events.key)
+for value in event_chance:
+    if res == event_chance.values():
+        print(event_chance.key)
