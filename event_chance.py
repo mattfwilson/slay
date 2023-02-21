f29b7326edf0ebff6a060017353367f6cd808296
mattfwilson <mattfwilson@gmail.com>
2023-02-21 06:27:20 -0500

    Fixed dict key properly printing when corresponding value is selected in trial loop

--- event_chance.py
+++ event_chance.py
@@ -4,5 +4,7 @@ event_chance = {'Event': 64, 'Shrine': 21, 'Fight': 10, 'Shop': 3, 'Treasure': 2
 trials = 100
 
 for trial in range(0, trials):
-    res = random.randint(0, 100)
-    print(res)
+    for key, value in event_chance.items():
+        roll = random.randint(0, 100)
+        if roll <= value:
+            print(key)
