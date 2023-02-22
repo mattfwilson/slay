7fd20456686b4a5cf56ff7b392968e9a7442ecdc
mattfwilson <mattfwilson@gmail.com>
2023-02-22 01:40:32 -0500

    Playing around with returning tuples values in dict when key is a tuple

--- event_chance.py
+++ event_chance.py
@@ -1,10 +1,31 @@
 import random
 
-event_chance = {'Event': 64, 'Shrine': 21, 'Fight': 10, 'Shop': 3, 'Treasure': 2}
-trials = 100
+event_chance = {
+    'Event': 64,
+    'Shrine': 21,
+    'Fight': 10,
+    'Shop': 3,
+    'Treasure': 2
+}
+
+test_dict = {
+    None: 'a',
+    (1, 2): 'b',
+    True: 'c'
+}
+
+trials = 25
 
 for trial in range(0, trials):
     for key, value in event_chance.items():
-        roll = random.randint(0, 100)
+        roll = random.randint(1, 100)
         if roll <= value:
             print(key)
+
+my_tuple = ((1, 1), (2, 2), (1, 2))
+
+for key in test_dict:
+    if (key == (1, 2)):
+        print(test_dict.get(key))
+    else:
+        print('Not in dictionary!')
