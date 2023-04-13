f5ebc45928cd1fcc37861c15a1788910b93eeb92
mattfwilson <mattfwilson@gmail.com>
2023-04-13 01:14:15 -0400

    Test for upgraded vs non-upgraded card

--- card_reward.py
+++ card_reward.py
@@ -7,11 +7,11 @@ class DefectCard:
         self._class = 'Defect'
 
 class BallLightning(DefectCard):
-    def __init__(self, upgrade):
+    def __init__(self, upgraded):
         super().__init__()
         self._name = ('Ball Lightning', 'Ball Lightning +1')
         self.type = 'Common'
-        self._upgrade = upgrade
+        self._upgrade = upgraded
         self._energy = 1
         self._damage = 7
         self._block = 0
@@ -22,10 +22,13 @@ class BallLightning(DefectCard):
         if self._upgrade == True:
             self._damage += 3
         self._desc = f'Deal {self._damage} damage. Channel {self._channel_amount[0]} {self._channel_type}.'
-    
+
     def showCard(self):
         print(f'{self._name[0]} ({self._energy})')
         print(f'{self._desc}')
 
-card1 = BallLightning(True)
+card1 = BallLightning(False)
 card1.showCard()
+
+card2 = BallLightning(True)
+card2.showCard()
