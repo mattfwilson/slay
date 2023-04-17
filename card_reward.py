8ac7b91b231d3617da59e4ee3838518883bc912a
mattfwilson <mattfwilson@gmail.com>
2023-04-17 00:51:39 -0400

    Added Cold Snap card and revised how titles change to +1 when upgraded

--- card_reward.py
+++ card_reward.py
@@ -9,7 +9,7 @@ class DefectCard:
 class BallLightning(DefectCard):
     def __init__(self, upgraded=False):
         super().__init__()
-        self._name = ('Ball Lightning', 'Ball Lightning +1')
+        self._name = 'Ball Lightning'
         self.type = 'Common'
         self._upgrade = upgraded
         self._energy = 1
@@ -20,18 +20,42 @@ class BallLightning(DefectCard):
         self._exhaust = False
         self._power = False
         if self._upgrade == True:
+            self._name = 'Ball Lightning +1'
             self._damage += 3
         self._desc = f'Deal {self._damage} damage. Channel {self._channel_amount[0]} {self._channel_type}.'
 
     def showCard(self):
-        print(f'{self._name[0]} ({self._energy}💧) - ID: {self._id}')
+        print(f'\n{self._name} ({self._energy}💧) - ID: {self._id}')
         print(f'{self._desc}')
 
+class ColdSnap(DefectCard):
+    def __init__(self, upgraded=False):
+        super().__init__()
+        self._name = 'Cold Snap'
+        self.type = 'Common'
+        self._upgrade = upgraded
+        self._energy = 1
+        self._damage = 6
+        self._block = 0
+        self._channel_type = 'Frost'
+        self._channel_amount = (1, 1)
+        self._exhaust = False
+        self._power = False
+        if self._upgrade == True:
+            self._name = 'Cold Snap +1'
+            self._damage += 3
+        self._desc = f'Deal {self._damage} damage. Channel {self._channel_amount[0]} {self._channel_type}.'
 
-    damage = property()
+    def showCard(self):
+        print(f'\n{self._name} ({self._energy}💧) - ID: {self._id}')
+        print(f'{self._desc}')
+
 
 card1 = BallLightning(False)
 card1.showCard()
 
 card2 = BallLightning(True)
 card2.showCard()
+
+card3 = ColdSnap(True)
+card3.showCard()
