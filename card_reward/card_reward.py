1f93ebc00452f65124defb33971f1cf98fed33a4
mattfwilson <mattfwilson@gmail.com>
2023-04-25 05:08:18 -0400

    More debugging for why upgrade conditional is not functioning properly

--- card_reward/card_reward.py
+++ card_reward/card_reward.py
@@ -3,12 +3,14 @@ from defect_cards import *
 
 drawCount = 10
 upgradeChoice = (True, False)
-upgraded = random.choice(upgradeChoice)
+upgraded = False
 defectCards = [BallLightning(upgraded), ColdSnap(upgraded)]
 hand = []
 
 for count in range(drawCount):
     print(count)
+    print(upgraded)
+    upgraded = random.choice(upgradeChoice)
     hand.append(random.choice(defectCards))
 
 for card in hand:
