d30d380391e67ac0468a6747a61809e9808867b7
mattfwilson <mattfwilson@gmail.com>
2023-04-26 01:34:23 -0400

    Simplified boolean randomization

--- card_reward/card_reward.py
+++ card_reward/card_reward.py
@@ -2,16 +2,15 @@ import random
 from defect_cards import *
 
 drawCount = 10
-upgradeChoice = (True, False)
-upgraded = False
+upgraded = random.choice([True, False])
 defectCards = [BallLightning(upgraded), ColdSnap(upgraded)]
 hand = []
 
 for count in range(drawCount):
     print(count)
     print(upgraded)
-    upgraded = random.choice(upgradeChoice)
-    hand.append(random.choice(defectCards))
+    draw = random.choice(defectCards)
+    res = hand.append(draw)
 
 for card in hand:
     print(card)
