c564804273cc59bb256986f48e374cc40a63dadf
mattfwilson <mattfwilson@gmail.com>
2023-05-10 00:09:25 -0400

    Converted old hand list to deck and created new hand list to draw into

--- card_reward/card_reward.py
+++ card_reward/card_reward.py
@@ -1,19 +1,27 @@
 import random
 from defect_cards import *
 
-drawCount = 10
+deckCount = 10
+drawCount = 5
 upgraded = False
+deck = []
 hand = []
 
 seed = random.seed(4)
 nested_seed = random.seed(seed)
 print(nested_seed)
 
-for count in range(drawCount):
+for count in range(deckCount):
     defectCards = [BallLightning(upgraded), ColdSnap(upgraded), Barrage(upgraded)]
     upgraded = random.choice([True, False])
     draw = random.choice(defectCards)
-    hand.append(draw)
+    deck.append(draw)
 
-for card in hand:
+for card in deck:
     print(card)
+
+for card in deck:
+    hand.append(card)
+print(f'Your hand: {hand}')
+
+    
