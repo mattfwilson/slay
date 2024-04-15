ef78a1fe28a28e6fba92f1dffb5b25d9f0d26564
mattfwilson <mattfwilson@gmail.com>
2024-04-15 00:48:43 -0400

    Using zip to loop over both lists

--- rampage.py
+++ rampage.py
@@ -3,6 +3,7 @@ plus_base = 8
 
 rampage = []
 rampage_plus = []
+delta = []
 
 for i in range(15):
     rampage.append(normal_base)
@@ -10,7 +11,10 @@ for i in range(15):
     rampage_plus.append(plus_base)
     plus_base += 8
 
-delta = [rampage_plus[i] - rampage[i] for i in range(len(rampage))]
+for i, j in zip(rampage_plus, rampage):
+  print(i, j)
+  diff = i - j
+  delta.append(diff)
 
 print(rampage)
 print(rampage_plus)
