75a49d584fb7b39de5d5670ba642524fb0455535
mattfwilson <mattfwilson@gmail.com>
2024-04-16 00:28:27 -0400

    Adding up the results to compare to chances

--- event_chance.py
+++ event_chance.py
@@ -8,24 +8,29 @@ event_chance = {
     'Treasure': 2
 }
 
-trials = 25
+trials = 100
+trial_res = {
+    'Event': 0,
+    'Shrine': 0,
+    'Fight': 0,
+    'Shop': 0,
+    'Treasure': 0
+}
 
 for trial in range(0, trials):
     for key, value in event_chance.items():
         roll = random.randint(1, 100)
         if roll <= value:
+            if key == 'Event':
+                trial_res['Event'] += 1
+            elif key == 'Shrine':
+                trial_res['Shrine'] += 1
+            elif key == 'Fight':
+                trial_res['Fight'] += 1
+            elif key == 'Shop':
+                trial_res['Shop'] += 1
+            elif key == 'Treasure':
+                trial_res['Treasure'] += 1
             print(key)
 
-my_tuple = ((1, 1), (2, 2), (1, 2))
-
-test_dict = {
-    None: 'a',
-    (1, 2): 'b',
-    True: 'c'
-}
-
-for key in my_tuple:
-    if (key == (1, 2)):
-        print(test_dict.get(key))
-    else:
-        print('Not in dictionary!')
+print(trial_res)
