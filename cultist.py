ddca49debb97300193e3df13362639a8a0343a7e
mattfwilson <mattfwilson@gmail.com>
2024-04-27 02:14:40 -0400

    Adding cultist enemy and reorganizing main slay file

---
+++ cultist.py
@@ -0,0 +1,48 @@
+import itertools
+import random
+
+class Enemy:
+    id = itertools.count(0)
+    def __init__(self):
+        self.id = next(Enemy.id)
+
+class Cultist(Enemy):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Cultist'
+        self._hp = random.randint(42, 48)
+        self.max_hp = self._hp
+
+    def __repr__(self):
+        return f'__repr__ for {self.name}, id: {self.id}'
+
+    def summary(self):
+        print(f'\n{self.name}')
+        print(f'🩸 HP: {self._hp}/{self.max_hp}')
+
+    def getName(self):
+        return self.name
+
+    def getHP(self):
+        return self._hp
+    
+    def doDamage(self, damage):
+        self.hp -= damage
+        return self.hp
+
+    def intent(self):
+        intents = [state.ACTIONS[2], state.ACTIONS[3]]
+        attackValues = [6, 8, 10, 14]
+        blockValues = [12, 14, 16, 18]
+        intent = random.randint(1, 2)
+        attack = random.choices(attackValues, weights=[1, 2, 6, 1])
+        for i in attack:
+            res = i
+        block = random.choices(blockValues, weights=[1, 8, 3, 2])
+        for i in block:
+            res = i
+        if intent == 1:
+            return intent, res
+        else:
+            return intent, res
+
---
+++ vars2.py
@@ -0,0 +1,48 @@
+class GameState:
+    def __init__(self):
+        self.NAME = 'Ironclad'
+        self.MAX_HP = 68
+        self.HP = self.MAX_HP
+        self.MAX_ENERGY = 3
+        self.ENERGY = self.MAX_ENERGY
+        self.DECK = []
+        self.START_ATTACK = 5
+        self.START_BLOCK = 5
+        self.START_DRAW = 2
+        self.DRAW_PILE = []
+        self.HAND = []
+        self.DISCARD_PILE = []
+        self.TURN_DRAW = 5
+        self.CARD_DRAW = 0
+        self.TURN_COUNT = 0
+        self.FLOOR_COUNT = 0
+        self.ENCOUNTER = []
+        self.ACTIONS = ['Draw', 'Attack', 'Block', 'Buff', 'Discard', 'Draw Pile', 'Discard Pile']
+        self.DEBUFFS = ['Vulnerable', 'Weakness', 'Frail']
+        self.ATTACK = 0
+        self.BLOCK = 0
+        self.ENEMY_HP = 0
+        self.ENEMY_INTENT = 0
+        self.ENEMY_BLOCK = 0 
+        self.ENEMY_ATTACK = 0
+        self.CARDS_PLAYED = 0
+
+def showDraw():
+    print(f'DRAW PILE:')
+    print(state.DRAW_PILE)
+
+def showHand():
+    print(f'\nCURRENT HAND:')
+    print(state.HAND)
+
+def showDiscard():
+    print(f'\nDISCARD PILE:')
+    print(state.DISCARD_PILE)
+
+def showPiles():
+    print('-' * 50 + f' [PILES]')
+    showDraw()
+    showHand()
+    showDiscard()
+
+state = GameState()
