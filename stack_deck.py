5d5eeef899507ef3663375695adc9d23d2ea7d21
mattfwilson <mattfwilson@gmail.com>
2024-06-10 01:29:26 -0400

    Debugging Survivor card not being calleable

--- stack_deck.py
+++ stack_deck.py
@@ -1,14 +1,21 @@
 import random
 
 card_type = ['Attack', 'Skill', 'Power']
+card_debuffs  = ['Vulnerable', 'Weakeness', 'Frail']
+card_abilities = ['Exhaust', 'Discard']
+
 
 class StrikeCard:
     def __init__(self, next_card=None):
+        self.name = 'Strike'
         self.type = card_type[0]
         self.energy = 1
         self.value = 6
         self.next_card = next_card
-    
+   
+    def get_name(self):
+        return self.name
+
     def get_type(self):
         return self.type
 
@@ -25,12 +32,16 @@ class StrikeCard:
         self.next_card = next_card
 
 class DefendCard:
-    def __init__(self, value=int(), next_card=None):
+    def __init__(self, next_card=None):
+        self.name = 'Defend'
         self.type = card_type[1]
         self.energy = 1
         self.value = 5
         self.next_card = next_card
     
+    def get_name(self):
+        return self.name
+
     def get_type(self):
         return self.type
 
@@ -46,6 +57,38 @@ class DefendCard:
     def set_next_card(self, next_card):
         self.next_card = next_card
 
+class SurvivorCard:
+    def __init__(self, next_card=None):
+        self.name = 'Survivor'
+        self.type = card_type[1]
+        self.energy = 1
+        self.value = 8
+        self.abilities = card_abilities[1]
+        self.next_card = next_card
+
+    def get_name(self):
+        return self.name
+    
+    def get_type(self):
+        return self.type
+
+    def get_energy(self):
+        return self.energy
+
+    def get_value(self) -> int:
+        return self.value
+
+    def get_abilities(self):
+        return self.abilities
+
+    def get_next_card(self):
+        return self.next_card
+
+    def set_next_card(self, next_card):
+        self.next_card = next_card
+
+base_cards = [SurvivorCard(), StrikeCard(), DefendCard()]
+
 class Stack:
     def __init__(self, value=None, limit=5):
         self.top_card = StrikeCard()
@@ -56,12 +99,16 @@ class Stack:
         return self.top_card
 
     def add_card(self):
-        if self.has_space():
-            new_card = random.choice([StrikeCard(), DefendCard()])
-            new_card.set_next_card(self.top_card)
+        if self.is_empty():
+            new_card = random.choices(base_cards, weights=[5, 3, 1])
+            print(new_card)
             self.top_card = new_card
         else:
-            print(f'Cannot add card!')
+            if self.has_space():
+                new_card = random.choices(base_cards, weights=[5, 3, 1])
+                new_card = random.choices(StrikeCard(), DefendCard(), SurvivorCard())
+                new_card.set_next_card(self.top_card)
+                self.top_card = new_card
 
     def remove_top_card(self):
         self.top_card = self.top_card.get_next_card()
@@ -76,7 +123,7 @@ class Stack:
         if self.size == 0:
             return True
 
-    def stringify(self) -> str:
+    def stringify(self):
         str_stack = ''
         top_card = self.peek()
         while top_card:
@@ -88,5 +135,7 @@ class Stack:
 stack = Stack(0)
 for i in range(5):
     stack.add_card()
+    print(stack.get_name())
+
 print(stack.stringify())
 
