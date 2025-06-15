7ce66c5e8ee47239f90a16e3bd51b01d108b3af8
mattfwilson <mattfwilson@gmail.com>
2025-06-15 10:59:43 -0400

    Floor deaths successfully parsing/graphing to matplot

--- parse_floors.py
+++ parse_floors.py
@@ -1,25 +1,23 @@
-from starters import starters
 from load_runs import load_run_history
 from matplotlib import pyplot as plt
+from collections import Counter
 
 characters = ['DEFECT']
 char_colors = {'IRONCLAD': 'red', 'THE_SILENT': 'green', 'DEFECT': 'skyblue', 'WATCHER': 'purple'}
-floor_reached: dict = {}
+floor_death: dict = {}
 
 for char in characters:
     runs = load_run_history(char)
      
     for filename, data in runs.items():
-        floor_reached = data.get('floors_reached')
-
-        for floor in floor_reached:
-            if data.get('victory') == True:
-                if floor not in floor_reached:
-                    floor_reached[floor] = 1
-                else:
-                    floor_reached[floor] += 1
-
+        floor_reached = data.get('floor_reached')
         print(floor_reached)
+
+        if floor_reached not in floor_death:
+            floor_death[floor_reached] = 1
+        else:
+            floor_death[floor_reached] += 1
+        
 
         if isinstance(data, dict):
             print('is dict')
@@ -31,7 +29,7 @@ for char in characters:
                 else:
                     print(f'[{filename} #{runid}] Invalid run format: {run}\n')
 
-    sorted_res: dict = dict(sorted(floor_reached.items(), key=lambda item: item[1], reverse=True))
+    sorted_res: dict = dict(sorted(floor_death.items(), key=lambda item: item[1], reverse=True))
 
     labels = list(sorted_res.keys())
     values = list(sorted_res.values())
@@ -46,9 +44,9 @@ for char in characters:
 
     plt.ylim (0, max_values * 1.05)
     plt.xticks(rotation=90, fontsize=8)
-    plt.title('Most Picked Relics in Successful Runs')
-    plt.xlabel('Relics Picks')
-    plt.ylabel('# Times Picked')
+    plt.title('Death by floor #')
+    plt.xlabel('Floor #')
+    plt.ylabel('Times died on floor')
     plt.tight_layout()
     plt.show()
 
