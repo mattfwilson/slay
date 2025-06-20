767df6f14a0a9ac66f41639b3a9e09ee8cc413d5
mattfwilson <mattfwilson@gmail.com>
2025-06-20 10:38:52 -0400

    Graphing all runs regardless of wins/losses

---
+++ parse_cards3.py
@@ -0,0 +1,54 @@
+from starters import starters
+from load_runs import load_run_history
+from matplotlib import pyplot as plt
+
+characters = ['DEFECT']
+char_colors = {'IRONCLAD': 'red', 'THE_SILENT': 'green', 'DEFECT': 'skyblue', 'WATCHER': 'purple'}
+card_picks: dict = {}
+
+for char in characters:
+    runs = load_run_history(char)
+     
+    for filename, data in runs.items():
+        print(f'{filename}\n{data}\n')
+        master_deck = data.get('master_deck')
+
+        for card in master_deck:
+            if card not in card_picks:
+                card_picks[card] = 1
+            else:
+                card_picks[card] += 1
+
+        if isinstance(data, dict):
+            print(f"Score: {data.get('score')} | Victory: {data.get('victory')} | Floor Reached: {data.get('floor_reached')} | Ascension Level: {data.get('ascension_level')}\n")
+        elif isinstance(data, list):
+            for i, run in enumerate(data):
+                if isinstance(run, dict):
+                    print('multiple embedded dicts')
+                else:
+                    print(f'[{filename} #{i}] Invalid run format: {run}\n')
+
+    sorted_res: dict = dict(sorted(card_picks.items(), key=lambda item: item[1], reverse=True))
+    sorted_res = {key: val for key, val in sorted_res.items() if key not in starters}
+
+    for key, val in sorted_res.items():
+            print(f'{key}: {val}')
+
+    labels = list(sorted_res.keys())
+    values = list(sorted_res.values())
+    max_values = max(values)
+    plt.figure(figsize=(20, 12))
+    
+    bars = plt.bar(labels, values, color=char_colors.get(char))
+    
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2, height, f'{int(height)}', ha='center', va='bottom')
+
+    plt.ylim (0, max_values * 1.05)
+    plt.xticks(rotation=90, fontsize=8)
+    plt.title('Most Picked Cards in Successful Runs')
+    plt.xlabel('Card Picks')
+    plt.ylabel('# Times Picked')
+    plt.tight_layout()
+    plt.show()
