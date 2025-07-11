dbefd50be59523b96d51f5115094a53834dc633c
mattfwilson <mattfwilson@gmail.com>
2025-07-10 23:11:28 -0400

    Parsing floors correctly with dictionaries

--- parse_floors2.py
+++ parse_floors2.py
@@ -2,23 +2,27 @@ from load_runs import load_run_history
 from load_runs import char_colors
 from matplotlib import pyplot as plt
 
-characters = ['DEFECT']
+characters = ['THE_SILENT']
 
 for char in characters:
     runs = load_run_history(char)
      
-    floor_reach_count = []
+    floor_reach_count = {}
 
     for filename, data in runs.items():
         floor_reached = int(data.get('floor_reached'))
 
         if floor_reached is None:
             continue
-        
-        floor_reach_count.append(floor_reached)
+        elif floor_reached not in floor_reach_count:
+            floor_reach_count[floor_reached] = 1
+        else:
+            floor_reach_count[floor_reached] += 1
 
-    labels = floor_reach_count
-    values = floor_reach_count
+    print(floor_reach_count.items())
+
+    labels = list(sorted(floor_reach_count.keys()))
+    values = [floor_reach_count[floor_reached] for floor_reached in labels]
 
     plt.figure(figsize=(20, 12))
     
@@ -29,9 +33,9 @@ for char in characters:
         plt.text(bar.get_x() + bar.get_width() / 2, height, f'{int(height)}', ha='center', va='bottom')
 
     plt.xticks(rotation=90, fontsize=8)
-    plt.title('History of deaths by floor #')
-    plt.xlabel('Run History')
-    plt.ylabel('Floor Died On')
+    plt.title(f'History of deaths by floor # ({sum(floor_reach_count.values())} total deaths on {char})')
+    plt.xlabel('Floor #')
+    plt.ylabel('Amount of Deaths')
     plt.tight_layout()
     plt.show()
     
